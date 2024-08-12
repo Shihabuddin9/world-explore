3c26c47664f1d5c96083dce3366092c5f4fed134
Shihabuddin9 <shihab.bd.pk@gmail.com>
2024-08-12 12:27:24 +0600

    Basic setup and add navbar

---
+++ src/component/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
+import { GiWorld } from "react-icons/gi";
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const links = [
+        { to: '/', label: 'Home' },
+        { to: '/menu', label: 'Menu' },
+        { to: '/mobileApp', label: 'Mobile app' },
+        { to: '/myOrders', label: 'My orders' }
+    ];
+
+    const renderLinks = () =>
+        links.map(({ to, label }) => (
+            <li key={to}>
+                <NavLink to={to} className='md:mx-5 my-5 md:my-0'>{label}</NavLink>
+            </li>
+        ));
+
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                {/* For mobile devices */}
+                <div>
+                    <div onClick={toggleMenu} tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+
+                    <div className={`fixed left-0 top-0 z-30 flex h-screen w-full bg-white shadow-lg transition-transform duration-500 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+                        <ul onClick={toggleMenu} tabIndex={0} className="menu menu-sm dropdown-content mt-10 ml-5 z-[1] p-2">
+                            {renderLinks()}
+                        </ul>
+                        <button onClick={toggleMenu} className="top-5 text-3xl absolute right-10">X</button>
+                    </div>
+                </div>
+
+                <Link to='/' className="text-xl font-serif flex justify-center items-center">
+                    <GiWorld className="text-4xl" />
+                    <span className="text-3xl text-color">
+                        <span>World</span>explore
+                    </span>
+                </Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {renderLinks()}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
